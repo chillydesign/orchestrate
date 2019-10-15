15d22aaead40dc12b727d08061f5fd5944feee18
Charles Harvey <harvey.charles@gmail.com>
2019-10-15 11:57:32 +0100

    add env variables for cpmdtdev

---
+++ src/environments/environment.cpmdt.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    site_name: `Orchestrate`,
+    api_url: 'https://webfactor.ch/cpmdtdev_api',
+    front_url: 'https://webfactor.ch/cpmdtdev/',
+    cookie_domain: 'webfactor.ch',
+    cookie_length_hours: 50,
+    cache_duration: 60000, // time to hold resources from API in cache in milliseconds
+};
